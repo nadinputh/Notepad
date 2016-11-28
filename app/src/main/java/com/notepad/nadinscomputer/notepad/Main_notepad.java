90ff8ce7af787e4bf3d4fd5bed578d1bd581db32
Puth Nadin <puthnadine@gmail.com>
2016-11-28 16:50:47 +0700

    Initial commit

---
+++ app/src/main/java/com/notepad/nadinscomputer/notepad/Main_notepad.java
@@ -0,0 +1,14 @@
+package com.notepad.nadinscomputer.notepad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main_notepad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_notepad);
+    }
+
+}
